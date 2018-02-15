b4a1d6d954d3e9e26efc1a17b1f71d6172e7b227
Karly <karlyhoffman@gmail.com>
2018-02-15 07:40:49 -0600

    Page 10 — projects section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,10 @@
 (function ($, window, document, undefined) {
-
   'use strict';
-
   $(function () {
-    
-  });
 
+    // Page 10 Scripts
+
+    // when user clicks a project in navbar, scroll to project section then open clicked project
+
+  });
 })(jQuery, window, document);
