d5fad12e4a29fa2e110e7bb19e04a9586537ac58
Karly <karlyhoffman@gmail.com>
2018-03-04 16:45:12 -0600

    Page 10 — animation timing

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -39,7 +39,7 @@
 
     $( ".reveal" ).each(function( index, elem ) {
      var revealIn = TweenMax.fromTo(this, 0.4, { "opacity": "0", marginTop: 40 }, { "opacity": "1", marginTop:0, ease: Power0.easeNone });
-     var revealScene = new ScrollMagic.Scene({triggerHook: "0.8", triggerElement: this}).setTween(revealIn).addTo(controller);
+     var revealScene = new ScrollMagic.Scene({triggerHook: "0.85", triggerElement: this}).setTween(revealIn).addTo(controller);
     });
 
 
