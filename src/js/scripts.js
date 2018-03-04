87604a0ecbf803b08ff9eed98efec765e050e2a1
Karly <karlyhoffman@gmail.com>
2018-03-04 14:36:07 -0600

    Page 10 — animations

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,45 @@
 
     /*** Page 10 Scripts ***/
 
+    // Intro animations
+    var tl_options = {
+      delay: 0,
+      stagger: 0,
+      autoRemoveChildren: false,
+      smoothChildTiming: false,
+      repeat: 0,
+      yoyo: false
+    };
+
+    var tl     = new TimelineMax(tl_options),
+        $nav   = $('#navigation'),
+        $about = $('#about'),
+        $projects = $('#projects');
+
+    tl.fromTo($nav, 0.75,
+      { rotation: 0 },
+      { rotation: -90, ease: Quad.easeInOut }
+    ).fromTo($about, 0.75,
+      { marginTop: 150, "opacity": "0" },
+      { marginTop: 0, "opacity": "1", ease: Power1.easeOut }
+    ).fromTo($projects, 0.75,
+      { marginTop: 10, "opacity": "0" },
+      { marginTop: 0, "opacity": "1", ease: Power1.easeOut }
+    );
+
+    // Scroll reveal animation
+    var controller = new ScrollMagic.Controller({
+      globalSceneOptions: {
+        reverse: false
+      }
+    });
+
+    $( ".reveal" ).each(function( index, elem ) {
+     var revealIn = TweenMax.fromTo(this, 0.4, { "opacity": "0", marginTop: 40 }, { "opacity": "1", marginTop:0, ease: Power0.easeNone });
+     var revealScene = new ScrollMagic.Scene({triggerHook: "0.8", triggerElement: this}).setTween(revealIn).addTo(controller);
+    });
+
+
     // navbar scroll
     var navLinks = $('#navigation a');
     navLinks.click( function(e) {
@@ -31,7 +70,7 @@
       $(this).removeClass('hover');
     })
 
-    // modal triggers
+    // modal
     var modalTriggers = $('.trigger');
     modalTriggers.click( function(e) {
       e.preventDefault();
