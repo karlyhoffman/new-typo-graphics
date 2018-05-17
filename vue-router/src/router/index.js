7239ef3acdb2b6a311369d97e40fd88712973b32
Karly <karlyhoffman@gmail.com>
2018-05-16 19:28:35 -0500

    Setup initial page route to first design

--- vue-router/src/router/index.js
+++ vue-router/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Gallery from '@/components/Gallery'
+import Page10 from '@/components/Page10/Page10.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Gallery',
+      component: Gallery
+    },
+    {
+      path: '/snap-design',
+      name: 'Page10',
+      component: Page10
     }
   ]
 })
